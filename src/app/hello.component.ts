6448f68c368b55448d7a01da815085ba204ffcfd
pavenkostanislav <pavenko_sv@mail.ru>
2022-02-14 10:20:27 +0300

    1 file modified: fixed token

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import twilio from 'twilio';
 const ACCOUNT_SID = 'AC009763bc133c85d8a0ae4362662e2165';
-const AUTH_TOKEN = '154a2d06f833b6c497efad38e28dcdd1';
+const AUTH_TOKEN = '';
 const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 const twiml = twilio.twiml.MessagingResponse();
 
